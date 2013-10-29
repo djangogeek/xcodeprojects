27a2899f3267ff19f416ede8ea8fa3c3e0a505b3
djangogeek <laurent.benedetti@gmail.com>
2013-10-29 10:10:52 +0100

    creation projet opis vobjet

---
+++ opsis_vobjet/opsis_vobjet/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  opsis_vobjet
+//
+//  Created by laurent on 29/10/13.
+//  Copyright (c) 2013 laurent Benedetti. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
